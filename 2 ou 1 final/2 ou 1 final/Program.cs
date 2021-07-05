e87a2a096234ae332a70c4e884f0a4ebab64c52b
BRUNO ROCHA MANTOVANI <Bruno.trabalho449243@outlook.com>
2021-07-04 21:19:53 -0300

    FAREI O PROGRAMA DE UMA OUTRA FORMA

--- 2 ou 1 final/2 ou 1 final/Program.cs
+++ 2 ou 1 final/2 ou 1 final/Program.cs
@@ -5,23 +5,20 @@ using System.Text;
 namespace _2_ou_1_final {
     class Program {
         static void Main(string[] args) {
-            Console.Write("INFORME A QUANTIDADE DE JOGADORES....");
+
+            Console.Write("INFORME A QUANTIDADE DE PARTICIPANTES..");
             int part = int.Parse(Console.ReadLine());
-            Jogador Diferente;
-            Jogador[] Vect = new Jogador[part];
-            for (int i = 0; i<part; i++) {
-                Console.Write("INFORME O NOME DO PARTICIPANTE....");
+            Jogador[,] Mat = new Jogador[part, part];
+          for (int i = 0; i<part; i++) {
+                Console.Write("INFORME O NOME DO PARTICIPANTE...");
                 string nome = Console.ReadLine();
-                int id = new Random().Next(1, 3);
-                Vect[i] = (new Jogador { Nome = nome, Resultado = id });
+                int val = new Random().Next(1, 2);
+                var row = new string[]; 
                 
-            }
-            for (int i = 0; i < part; i++) {
-                 if (Vect [i] != Vect [i] && Vect[i] == Vect[i]) {
-                    Diferente = Vect[i];
+                for (int j = 0; j<part; j++) {
+
                 }
             }
-            Console.WriteLine(Diferente);
         }
     }
 }
