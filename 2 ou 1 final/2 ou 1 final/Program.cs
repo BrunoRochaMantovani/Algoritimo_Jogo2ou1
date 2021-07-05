7c8e35ebcb57e3e865ec427af7c76b1891b4c192
BRUNO ROCHA MANTOVANI <Bruno.trabalho449243@outlook.com>
2021-07-04 22:55:40 -0300

    FEITO ALTERAÇÕES NO PROGRAMA, ESTÁ FUNCIONANDO CONFORME AS EXPECTATIVAS, A MAIOR PARTE DO PROCESSO É FEITO ATRAVÉS DO METODO FUNCIONALIDADE, NÃO ESTÁ PERFEITO MAS FUNCIONA DE ACORDO COM O ESPERADO, FAREI NOVAS ALTERAÇÕES EM BREVE

--- 2 ou 1 final/2 ou 1 final/Program.cs
+++ 2 ou 1 final/2 ou 1 final/Program.cs
@@ -8,16 +8,42 @@ namespace _2_ou_1_final {
 
             Console.Write("INFORME A QUANTIDADE DE PARTICIPANTES..");
             int part = int.Parse(Console.ReadLine());
-            Jogador[,] Mat = new Jogador[part, part];
-          for (int i = 0; i<part; i++) {
-                Console.Write("INFORME O NOME DO PARTICIPANTE...");
+            Funcionalidade(part);
+            Console.WriteLine("GERAR NOVAMENTE ? (y/n)");
+            char Escolha = char.Parse(Console.ReadLine());
+            while (Escolha == 'y' || Escolha == 'Y') {
+                Funcionalidade(part);
+                Console.WriteLine("GERAR NOVAMENTE ? (y/n)");
+                Escolha = char.Parse(Console.ReadLine());
+            }
+        }
+        public static void Funcionalidade(int Part) {
+            List<Jogador> mainlist = new List<Jogador>();
+            List<Jogador> List01 = new List<Jogador>();
+            List<Jogador> List02 = new List<Jogador>();
+            for (int i = 0; i < Part; i++) {
+                Console.Write("INFORME O NOME DO JOGADOR...");
                 string nome = Console.ReadLine();
-                int val = new Random().Next(1, 2);
-                var row = new string[]; 
-                
-                for (int j = 0; j<part; j++) {
+                int numero = new Random().Next(1, 3);
+                mainlist.Add(new Jogador { Nome = nome, Resultado = numero });
+            }
+            foreach (Jogador Obj in mainlist) {
+                int Ref = 1;
 
+                if (Obj.Resultado != Ref) {
+                    List02.Add(new Jogador { Nome = Obj.Nome, Resultado = Obj.Resultado });
+                } else {
+                    List01.Add(new Jogador { Nome = Obj.Nome, Resultado = Obj.Resultado });
                 }
+
+            }
+            foreach (Jogador Obj in List01) {
+                Console.WriteLine("\nJOGADORES QUE ESCOLHERAM O 1: " + Obj.Nome + " ESCOLHA: " + Obj.Resultado + "\n");
+
+            }
+            foreach (Jogador Obj in List02) {
+                Console.WriteLine("\nJOGADORES QUE ESCOLHERAM O 2: " + Obj.Nome + " ESCOLHA: " + Obj.Resultado + "\n");
+
             }
         }
     }
